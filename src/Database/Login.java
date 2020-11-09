9605d8953ff9f7459f458ca133d8681b4530c056
Nigam <w0752460@myscc.ca>
2020-11-08 22:57:53 -0500

    Created Databse package In Login just added DB_NAME,USER,PASSWORD

---
+++ src/Database/Login.java
@@ -0,0 +1,7 @@
+package Database;
+
+public class Login {
+    public static final String DB_NAME = "npateldb";
+    public static final String DB_USER = "npatel";
+    public static final String DB_PASSWORD = "";
+}
