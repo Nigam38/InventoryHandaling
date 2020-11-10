2b0808c071659378cf517ff0e9c65cf8f0af5e3a
Nigam Patel <w0752460@myscc.ca>
2020-11-10 15:25:52 -0500

    Created classes with appropriate packages * It will display empty screen

---
+++ src/Main/ScreenSize.java
@@ -0,0 +1,8 @@
+package Main;
+
+public class ScreenSize {
+    public final static int ScreenX = 1024;
+    public final static int ScreenY = 768;
+
+    public final static String title = "Inventory Handling";
+}
---
+++ src/Main/StartMain.java
@@ -0,0 +1,22 @@
+package Main;
+
+import Scene.WelcomeScene;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class StartMain extends Application {
+
+    public static Stage mainStage;
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        mainStage = primaryStage;
+        mainStage.setScene(new WelcomeScene());
+        mainStage.setTitle(ScreenSize.title);
+        mainStage.show();
+    }
+}
